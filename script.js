bd51885fb367cdb750fc6d61218746d093bd2222
tonamao <46984598+tonamao@users.noreply.github.com>
2019-03-03 16:37:11 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+(function(){
+  'use strict'
+  var pen = document.getElementById('pen');
+  var eraser = document.getElementById('eraser');
+  var clear = document.getElementById('clear');
+  
+  var cnv = document.getElementById('canvas');
+  var ctx = cnv.getContext('2d');
+  
+  var clickFlg = 0;
+  var penMode = 0;//0->pen, 1->eraser
+  
+  cnv.addEventListener('mousedown', onClick, false);
+  cnv.addEventListener('mousemove', draw, false);
+  cnv.addEventListener('mouseup', mouseUp, false);
+  cnv.addEventListener('mouseout', mouseUp, false);
+  
+  pen.addEventListener('click', penClick, false);
+  eraser.addEventListener('mousedown', erase, false);
+  clear.addEventListener('mousedown', clearCanvas, false);
+  
+  
+  function onClick(e){
+    clickFlg = 1;
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - rect.left, e.clientY - rect.top);
+    
+  }
+  
+  function draw(e){
+    
+    if(clickFlg == 1){
+      
+      var rect = e.target.getBoundingClientRect();
+      var x = e.clientX - rect.left;
+      var y = e.clientY - rect.top;
+
+      ctx.lineTo(x, y);
+      ctx.lineWidth = 5;
+      if(penMode == 0){
+        ctx.strokeStyle = '#2d3436';
+      }else if(penMode == 1){
+        ctx.strokeStyle = 'white';
+      }
+      ctx.stroke();
+    };
+  }
+  
+  function mouseUp(){
+    clickFlg = 0;
+  }
+  
+  function penClick(){
+    penMode = 0;
+  }
+  
+  function erase(){
+    penMode = 1;
+  }
+  
+  function clearCanvas(){
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.globalAlpha = 1.0;
+    ctx.fillRect(0, 0, 500, 500);
+    
+    penMode = 0;
+  }
+  
+  
+})();
